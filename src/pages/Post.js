70b4d7f344136034013b5914e58576eaf2475aec
gabriel6181997 <gabriel6181997@gmail.com>
2021-10-17 23:26:07 +0900

    ✨ Add a function for editing posts

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -78,12 +78,58 @@ export const Post = () => {
       });
   };
 
+  const editPost = (option) => {
+    if (option === "title") {
+      let newTitle = prompt("Enter new title");
+      axios.put(
+        `http://localhost:3001/posts/title`,
+        { newTitle, id },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      );
+      setPostObject({ ...postObject, title: newTitle });
+    } else {
+      let newPostText = prompt("Enter new text");
+      axios.put(
+        `http://localhost:3001/posts/postText`,
+        { newText: newPostText, id },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      );
+      setPostObject({ ...postObject, postText: newPostText });
+    }
+  };
+
   return (
     <div className="postPage">
       <div className="leftSide">
         <div className="post" id="individual">
-          <div className="title">{postObject.title}</div>
-          <div className="body">{postObject.postText}</div>
+          <div
+            className="title"
+            onClick={() => {
+              if (authState.username === postObject.username) {
+                editPost("title");
+              }
+            }}
+          >
+            {postObject.title}
+          </div>
+          <div
+            className="body"
+            onClick={() => {
+              if (authState.username === postObject.username) {
+                editPost("body");
+              }
+            }}
+          >
+            {postObject.postText}
+          </div>
           <div className="footer">
             {postObject.username}
             {authState.username === postObject.username ? (
